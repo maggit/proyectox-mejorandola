cc76d538953c356a18dcc6367c1058e2495d4be5
Raquel Hernandez <maggit@maggit.net>
2014-08-20 14:30:09 -0400

    More changes to demo

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
   Sidekiq.configure_server do |config|
-    config.redis = { :url => ENV["REDISTOGO_URL"] , :namespace => 'glimpse' }
+    config.redis = { :url => ENV["REDISTOGO_URL"] , :namespace => 'mejorandola' }
     db_config = YAML.load_file("#{Rails.root}/config/database.yml")[Rails.env]
     db_config["pool"] = 25
     ActiveRecord::Base.establish_connection(db_config)
